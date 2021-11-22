2881d2ac94b769d27f3db6dffb00ab1f7e283c3b
Felix Schlegel <schlegel.felix@gmail.com>
2021-11-22 14:36:13 +0100

    added testing example

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,12 +5,15 @@ from brownie import accounts, config, SimpleStorage
 def deploy_simple_storage():
     account = accounts[0]
     simple_storage = SimpleStorage.deploy({"from": account})
-    stored_value = simple_storage.retrieve() # since its a "view" function, doesnt need "from"
+    # since its a "view" function, doesnt need "from"
+    stored_value = simple_storage.retrieve()
     print(stored_value)
-    transaction = simple_storage.store(15, {"from": account}) #since its transaction, need from
-    transaction.wait(1) # how many blocks to wait, for transaction to finish
+    # since its transaction, need from
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)  # how many blocks to wait, for transaction to finish
     updated_store_value = simple_storage.retrieve()
     print(updated_store_value)
+
 
 def main():
     deploy_simple_storage()
